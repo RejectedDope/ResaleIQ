a00d513e22ca895382233a8ef35f99814b647fc5
FlipFlow <resaleiq@outlook.com>
2026-04-28 06:01:59 -0400

    Add sample inventory data

---
+++ lib/sampleData.ts
@@ -0,0 +1,13 @@
+export const sampleDeadListings = [
+  {
+    item: 'Coach Leather Tote',
+    platform: 'eBay',
+    age: 94,
+    price: 85,
+    complianceScore: 62,
+    profitScore: 48,
+    deadRisk: 'High',
+    topIssue: 'Weak title and stale listing',
+    recommendedAction: 'Relist'
+  }
+];
